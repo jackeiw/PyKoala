8c2ce297ad827616e03400b3aa8beaef03421367
jackeiw <jackeiw@163.com>
2018-06-29 13:29:37 +0800

    22

---
+++ test.py
@@ -0,0 +1,19 @@
+print ("you NB")
+
+if 10 > 210:
+    print ("greater than!")
+    print ("right!")
+else:
+    print ("less than!")
+    pass
+
+a = 100
+b = 1000
+print (a+b)
+
+print('shit') 
+
+
+
+
+
