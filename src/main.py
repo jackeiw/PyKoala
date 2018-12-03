30b23f91179b8ae4b997f65c4fd502d78f9d12e7
Vincent on T410 <jackeiw@163.com>
2018-12-03 22:23:58 +0800

    test

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
-print ("you NB")
+print("you NB")
 
 if 10 > 210:
-    print ("greater than!")
-    print ("right!")
+    print("greater than!")
+    print("right!")
 else:
     print ("less than!")
     pass
 
 a = 100
 b = 1000
-print (a+b)
+print(a+b)
 
 print('shit')
 
