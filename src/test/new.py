f87667693e2d0e03a9c7e41d7b32355768d9eea2
vincent <jackeiw@163.com>
2019-04-22 22:10:02 +0800

    迭代器使用

--- src/test/new.py
+++ src/test/new.py
@@ -1,24 +1,31 @@
+#迭代使用
 #获取最大最小值
-def findMinadnMax(x):
-    if x.length < 1:
+def findMinAndMax(x):
+    if not isinstance(x, list):
+        return TypeError('Invalid')
+    x_num = [c for c in x if type(c) is int]
+    if len(x_num) == 0:
         return (None, None)
-    else:
-        min = max= x[0]
-        for num in x:
-            if num > max:
-                max = num
-            else:
-                min = num
-        return (min, max)
+    min = x[0]
+    max = x[0]
+    for num in x:
+        if num > max:
+            max = num
+        else:
+            min = num
+    return (min, max)
 
-    # 测试
-    if findMinadnMax([]) != (None, None):
-        print('测试失败!')
-    elif findMinadnMax([7]) != (7, 7):
-        print('测试失败!')
-    elif findMinadnMax([7, 1]) != (1, 7):
-        print('测试失败!')
-    elif findMinadnMax([7, 1, 3, 9, 5]) != (1, 9):
-        print('测试失败!')
-    else:
-        print('测试成功!')
+'''
+# 测试
+if findMinAndMax([]) != (None, None):
+    print('测试失败!')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失败!')
+else:
+    print('测试成功!')
+'''
+
