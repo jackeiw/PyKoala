58630dafc1b4c8fd117dda1c0d7b40027304a857
Vincent on T410 <jackeiw@163.com>
2019-05-05 09:28:32 +0800

    test

---
+++ src/test/new.py
@@ -0,0 +1,14 @@
+testname = '我是Carl'
+print(testname.capitalize())
+
+testname = 'hello carl!'
+print(testname.capitalize())
+
+print('我是中国人\n'*8)
+
+print(len('我是中国人s'))
+
+print(testname.find('ll'))
+#import this
+
+help(type(testname))
