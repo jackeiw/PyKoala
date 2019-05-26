866e576434b74be220e5e1ed9e5ff5280f6bce53
Vincent on T410 <jackeiw@163.com>
2019-05-27 07:28:14 +0800

    作为Python学习手册的测试代码

---
+++ src/test/xxsc/script1.py
@@ -0,0 +1,13 @@
+# A first Python Script
+import sys
+print(sys.platform)
+print(2**100)
+s = 'Spam!'
+print(s * 18)
+
+import myfile
+print(myfile.title)
+
+from myfile import title
+print(title)
+
