84877caa1a09daaf462b6fb0071686a396cba780
Vincent on T410 <jackeiw@163.com>
2020-04-10 18:26:30 +0800

    添加给徐的代码

--- src/app.py
+++ src/app.py
@@ -1,1 +1,5 @@
 print('hello python!')
+
+for i in range(11):
+    print('何书桓走的第'+str(i+1)+'天，想他')
+    print('%s走的第%d天，想他' % ('书桓', i+1))
---
+++ src/test/xjt/study.py
@@ -0,0 +1,30 @@
+# 导入 random(随机数) 模块
+import random
+
+#a=random.randint(0,9)
+#print(str(a))
+
+man = 0
+count = 0
+while count < 10:
+    man = random.randint(0, 1000) #取0~1000的随机数，可以先不管
+    #print('一条好汉出生了：'+str(man))
+    count = count + 1
+    if count >= 10: #过了10次机会不没了
+        break
+    if man > 100 and man % 3 == 0 and man % 5 == 0 and man % 2 == 0: #愿意为你死
+        break
+
+if count < 10:
+    print('Goodbye! 经过' + str(count) + '次碰撞，他 ['+ str(man)+'] 来了。祝你和' + str(man) + '幸福~')
+else:
+    print("注定孤独终老， o(╥﹏╥)o")
+
+var = 1
+while var == 1:  # 该条件永远为true，循环将无限执行下去
+    num = input("Enter a number  :") #输入一个数
+    print("You entered: ", num)
+    if num > 100:
+        break
+
+print("Good bye!")
