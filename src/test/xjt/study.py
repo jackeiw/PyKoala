0b00ff8ebe308c267c92fe8ea6368143319e887f
Vincent on T410 <jackeiw@163.com>
2020-04-13 11:20:37 +0800

    添加给徐的代码

--- src/test/xjt/study.py
+++ src/test/xjt/study.py
@@ -7,12 +7,12 @@ import random
 man = 0
 count = 0
 while count < 10:
-    man = random.randint(0, 1000) #取0~1000的随机数，可以先不管
-    #print('一条好汉出生了：'+str(man))
+    man = random.randint(0, 1000) # 取0~1000的随机数，可以先不管
+    # print('一条好汉出生了：'+str(man))
     count = count + 1
-    if count >= 10: #过了10次机会不没了
+    if count >= 10: # 过了10次机会不没了
         break
-    if man > 100 and man % 3 == 0 and man % 5 == 0 and man % 2 == 0: #愿意为你死
+    if man > 100 and man % 3 == 0 and man % 5 == 0 and man % 2 == 0: # 愿意为你死
         break
 
 if count < 10:
@@ -20,9 +20,9 @@ if count < 10:
 else:
     print("注定孤独终老， o(╥﹏╥)o")
 
-var = 1
-while var == 1:  # 该条件永远为true，循环将无限执行下去
-    num = input("Enter a number  :") #输入一个数
+
+while True:  # 该条件永远为true，循环将无限执行下去
+    num = input("Enter a number  :") # 输入一个数
     print("You entered: ", num)
     if num > 100:
         break
